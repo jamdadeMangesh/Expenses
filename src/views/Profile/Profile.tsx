305290a485480a96580a2242111582dd5d569d4d
Mangesh Jamdade <Mangesh.Jamdade@theaccessgroup.com>
2024-03-19 14:15:05 +0530

    Show notification on PWA update

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -113,7 +113,7 @@ export const Profile = () => {
 				<div className="userWrapper__section shadow-sm px-3 py-2">
 					<div className="userWrapper__grid d-flex justify-content-between">
 						<div className="userWrapper__grid-header">App version</div>
-						<div className="userWrapper__grid-description">0.0.0.1</div>
+						<div className="userWrapper__grid-description">dev.0.0.0.1</div>
 					</div>
 				</div>
 				{role === "admin" && (
